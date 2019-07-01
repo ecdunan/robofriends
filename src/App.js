d93b4fdbd20cca1ba482a25517bd3e6fae5075b1
ecdunan <dunan.emmanuel@gmail.com>
2019-07-02 04:41:07 +0800

    Added fetch

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
-import { robots } from './robots';
+// import { robots } from './robots';
 import './App.css'
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots
+            robots : [],
+            searchTerm: '',
         };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response => response.json())
+          .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) => {
         const searchTerm = event.target.value;
-        const filteredArray = robots.filter(robot => {
-            const {name} = robot;
-            return name.toLowerCase().includes(searchTerm.toLowerCase());
-        });
 
         this.setState({
-            robots : filteredArray
+            searchTerm
         })
     }
 
     render() {
+        const filteredArray = this.state.robots.filter(robot => {
+            const { name } = robot;
+            return name.toLowerCase().includes(this.state.searchTerm.toLowerCase());
+        });
+
         return (
                 <div className ='tc'>
                     <h1 className='f1'>ROBOFRIENDS</h1>
                     <SearchBox onSearchChange={this.onSearchChange}/>
-                    <CardList robots={this.state.robots}/>
+                    <CardList robots={filteredArray}/>
                 </div>
             );
     }
