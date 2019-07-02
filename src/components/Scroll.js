7ae28b6d6420db9de2b2501ad60511067d5cbae7
ecdunan <dunan.emmanuel@gmail.com>
2019-07-02 14:52:20 +0800

    Adjusted Scroll height

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Scroll(props) {
     return (
-            <div style={{ overflowY: 'scroll', border: '3px solid black', height: '500px' }}>
+            <div style={{ overflowY: 'scroll', border: '3px solid black', height: '440px' }}>
                 {props.children}
             </div>
         );
